dc6c5e04c9b874d28fe5bee6e3412c0cdba5f550
albertofanini-prog <albertofanini@outlook.com>
2022-04-08 16:46:47 +0200

    Classe Movie, variabili di istanza

--- index.php
+++ index.php
@@ -2,6 +2,12 @@
 
     require_once __DIR__ . '/database.php';
 
-    var_dump("Film")
+    $film1 = new Movie('Paura e delirio a Las Vegas', 'Terry Gilliam', 1998)
+    $film2 = new Movie('Fast & Furious 6', 'Justin Lin', 2013)
+    $film3 = new Movie('Altrimenti ci arrabbiamo', 'Marcello Fondato', 1974)
+    $film4 = new Movie('Natale a Miami', 'Neri Parenti', 2005)
+    $film5 = new Movie('Spider-Man', 'Sam Raimi', 2002)
+
+    // var_dump("Film");
 
 ?>
