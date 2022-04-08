1c860e657055683e1af1f5e5dc61c7212c783893
albertofanini-prog <albertofanini@outlook.com>
2022-04-08 17:04:55 +0200

    Stampa statica delle istanze

--- database.php
+++ database.php
@@ -5,12 +5,17 @@
         public $author;
         public int $year;
 
-        function __construct($title){
+        function __construct($title, $author, $year){
             $this->title = $title;
+            $this->author = $author;
+            $this->year = $year;
         }
 
-        function get_title(){
-            return $this->title;
+        function get_info(){
+            return 
+            'Il titolo è: '.$this->title.'<br>'.
+            'Il regista è: '.$this->author.'<br>'.
+            'La data di pubblicazione è: '.$this->year.'<br>';
         }
     }
 
--- index.php
+++ index.php
@@ -9,5 +9,9 @@
     $film5 = new Movie('Spider-Man', 'Sam Raimi', 2002);
 
     // var_dump("Film");
-    echo $film1->get_title();
+    echo $film1->get_info();
+    echo $film2->get_info();
+    echo $film3->get_info();
+    echo $film4->get_info();
+    echo $film5->get_info();
 ?>
