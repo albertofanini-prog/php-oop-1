2e5b96222f9fb8a4807fde824d3cdd3390dcd191
albertofanini-prog <albertofanini@outlook.com>
2022-04-08 16:58:03 +0200

    Stampa primo titolo con construct e getTitle

--- database.php
+++ database.php
@@ -1,9 +1,17 @@
 <?php
 
     class Movie{
-        public String $title;
-        public String $author;
+        public $title;
+        public $author;
         public int $year;
+
+        function __construct($title){
+            $this->title = $title;
+        }
+
+        function get_title(){
+            return $this->title;
+        }
     }
 
 ?>
